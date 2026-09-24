4df0e3be2ccd8726ca264fcc5e985f4f30e7eac4
Sourabh Rawat <souravrawat444@gmail.com>
2026-09-24 23:17:45 +0530

    fix: update User model pre-save middleware to remove next parameter for Mongoose 9 compatibility

--- src/models/User.js
+++ src/models/User.js
@@ -33,13 +33,13 @@ const UserSchema = new mongoose.Schema({
 }, { timestamps: true });
 
 // Pre-save password hashing
-UserSchema.pre('save', async function (next) {
+// Mongoose 9 no longer passes `next` to middleware; async hooks just return
+UserSchema.pre('save', async function () {
   if (!this.isModified('password') || !this.password) {
-    return next();
+    return;
   }
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
-  next();
 });
 
 // Compare password helper
